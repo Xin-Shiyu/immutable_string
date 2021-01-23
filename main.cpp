30360746c1c24b93878105458e7b5eee24d7d6fc
Xin-Shiyu <xinshiyu@188.com>
2021-01-24 00:37:39 +0800

    remove main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "format.h"
-#include <stdio.h>
-
-int main()
-{
-	for (size_t i = 0; i != (size_t)-1; ++i)
-		puts(nativa::format("The statement that {0} is odd is {1}", i, i % 2 != 0).c_str());
-};
