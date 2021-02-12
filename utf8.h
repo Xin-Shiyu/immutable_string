c38c5b77df477915bee553f1b58bd0d8a38e5ef2
Xin-Shiyu <xinshiyu@188.com>
2021-02-12 18:14:40 +0800

    add utf8::encode(char32_t c, OutputIt out)

--- utf8.h
+++ utf8.h
@@ -59,6 +59,9 @@ namespace nativa
 
 				access_iterator m_end;
 			};
+		
+			template <typename OutputIt>
+			static void encode(char32_t c, OutputIt out);
 		};
 
 		inline utf8::access_iterator& utf8::access_iterator::operator++()
@@ -207,6 +210,43 @@ namespace nativa
 		inline utf8::accessor::accessor(const char* begin, const char* end)
 			: m_begin(utf8::access_iterator(begin)), m_end(utf8::access_iterator(end))
 		{
+		}
+	
+		template <typename OutputIt>
+		inline void utf8::encode(char32_t c, OutputIt out)
+		{
+			if (c <= U'\x007F')
+			{
+				*out = static_cast<uint8_t>(c);
+				++out;
+			}
+			else if (c <= U'\x07FF')
+			{
+				*out = static_cast<uint8_t>(0b11000000 | (static_cast<uint32_t>(c) >> 6));
+				++out;
+				*out = static_cast<uint8_t>(0b10000000 | (static_cast<uint32_t>(c) & 0b111111));
+				++out;
+			}
+			else if (c <= U'\xFFFF')
+			{
+				*out = static_cast<uint8_t>(0b11100000 | (static_cast<uint32_t>(c) >> 12));
+				++out;
+				*out = static_cast<uint8_t>(0b10000000 | ((static_cast<uint32_t>(c) >> 6) & 0b111111));
+				++out;
+				*out = static_cast<uint8_t>(0b10000000 | (static_cast<uint32_t>(c) & 0b111111));
+				++out;
+			}
+			else if (c <= U'\x1FFFF')
+			{
+				*out = static_cast<uint8_t>(0b11100000 | (static_cast<uint32_t>(c) >> 18));
+				++out;
+				*out = static_cast<uint8_t>(0b10000000 | ((static_cast<uint32_t>(c) >> 12) & 0b111111));
+				++out;
+				*out = static_cast<uint8_t>(0b10000000 | ((static_cast<uint32_t>(c) >> 6) & 0b111111));
+				++out;
+				*out = static_cast<uint8_t>(0b10000000 | (static_cast<uint32_t>(c) & 0b111111));
+				++out;
+			}
 		}
 	}
 }
